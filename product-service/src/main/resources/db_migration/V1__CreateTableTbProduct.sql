22a174c8262b2b4997e697509c654bc6755e13a6
Bernardo Antunes Heckler <bernaheckler1@gmail.com>
2025-10-31 22:16:06 -0300

    feat: Criando a WsProductControllers com a Autorização via JWT no Product-Service, o que ela faz? como estamos seguindo a ideia do CRUD, usuario com token de admin pode criar, ler, atualizar e deletar um Produto, foi realizada as requisições com Postman.

--- product-service/src/main/resources/db_migration/V1__CreateTableTbProduct.sql
+++ product-service/src/main/resources/db_migration/V1__CreateTableTbProduct.sql
@@ -1,7 +1,7 @@
 
 create table tb_product (
     id serial not null,
-    description varchar(100) not null,
+    description varchar(255) not null,
     brand varchar(255) not null,
     model varchar(255) not null,
     currency varchar(3) not null,
--- product-service/src/main/resources/db_migration/V3__AlterAndUpdateTableTbProduct.sql
+++ product-service/src/main/resources/db_migration/V3__AlterAndUpdateTableTbProduct.sql
@@ -5,5 +5,5 @@ ADD COLUMN image_url VARCHAR(255);
 -- Atualiza os registros existentes
 UPDATE tb_product SET image_url = 'https://example.com/images/iphone15.jpg' WHERE model = 'iPhone 15 256GB';
 UPDATE tb_product SET image_url = 'https://example.com/images/moto g85.jpg' WHERE model = 'G85 256GB';
-UPDATE tb_product SET image_url = 'https://example.com/images/redmi12c.jpg' WHERE model = 'Redmi 12C 256GB';
+UPDATE tb_product SET image_url = 'https://example.com/images/redmi12c.jpg' WHERE model = 'Redmi 13C 256GB';
 UPDATE tb_product SET image_url = 'https://example.com/images/s23ultra.jpg' WHERE model = 'S23 Ultra 256GB';
